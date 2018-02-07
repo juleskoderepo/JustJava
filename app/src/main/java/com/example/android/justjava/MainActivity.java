b17fcdf48583979ec709fd97a4585a629b667123
jbcruz <35581373+juleskoderepo@users.noreply.github.com>
2018-02-06 20:34:22 -0800

    Add name input and chocolate topping selection
    Allow user to enter name to display in the order summary rather than a
    static name.
    
    Uncheck toppings selected when Clear button is clicked.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -47,15 +49,23 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method is called when the order button is clicked.
+     *
+     * @param view
      */
     public void submitOrder(View view) {
+        EditText nameEditText = (EditText) findViewById(R.id.edittext_name_input);
+        String nameEntered = nameEditText.getText().toString();
+
         CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.checkbox_whippedcream);
         boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.checkbox_chocolate);
+        boolean hasChocloate = chocolateCheckbox.isChecked();
 
         int totalPrice = calculatePrice();
 
         if (quantity >= 1){
-            displayMessage(createOrderSummary(totalPrice, hasWhippedCream));
+            displayMessage(createOrderSummary(nameEntered, hasWhippedCream, hasChocloate, totalPrice));
         } else {
             String message = "Please try again. You must select at least 1 coffee to submit an order.";
 
@@ -72,11 +82,22 @@ public class MainActivity extends AppCompatActivity {
         return quantity * baseCoffeePrice;
     }
 
-    private String createOrderSummary(int totalPrice, boolean addWhippedCream){
-        String customerName = "Your Name Here";
+
+    /**
+     * This method is called to build the order summary message when
+     * the order button is clicked.
+     *
+     * @param custName
+     * @param addWhippedCream
+     * @param addChocolate
+     * @param totalPrice
+     * @return
+     */
+    private String createOrderSummary(String custName, boolean addWhippedCream, boolean addChocolate,int totalPrice){
         String orderSummary = "";
-        orderSummary += "Name: " + customerName;
+        orderSummary += "Name: " + custName;
         orderSummary += "\nAdd whipped cream?: " + addWhippedCream;
+        orderSummary += "\nAdd chocolate?: " + addChocolate;
         orderSummary += "\nQuantity: " + quantity;
         orderSummary += "\nTotal: " + NumberFormat.getCurrencyInstance().format(totalPrice);
         orderSummary += "\nThank you!";
@@ -92,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
 
         displayQuantity(quantity);
         displayPrice(price);
+        uncheckToppings();
     }
     /**
      * This method displays the given quantity value on the screen.
@@ -109,6 +131,23 @@ public class MainActivity extends AppCompatActivity {
         orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
+    /**
+     * This method clears the topping checkboxes
+     */
+    private void uncheckToppings(){
+        CheckBox whippedCreamTopping, chocolateTopping;
+
+        whippedCreamTopping = (CheckBox) findViewById(R.id.checkbox_whippedcream);
+        chocolateTopping = (CheckBox) findViewById(R.id.checkbox_chocolate);
+
+        if(whippedCreamTopping.isChecked()){
+            whippedCreamTopping.toggle();
+        }
+
+        if (chocolateTopping.isChecked()){
+            chocolateTopping.toggle();
+        }
+    }
 
     /**
      * This method displays the order summary message.
